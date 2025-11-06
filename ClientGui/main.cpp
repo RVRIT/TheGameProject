9457fb909a46a920015b2da18aa7c969ce5635ab
Chris2312-05 <cristi2005b@yahoo.com>
2025-11-06 15:25:21 +0200

    Clasa animator ( trebuie slefuita )
    Optiuni de a alege framerate, daca animatia isi da play in reverse dupa animatia principala, etc

--- ClientGui/main.cpp
+++ ClientGui/main.cpp
@@ -3,6 +3,7 @@
 #include "textBox.h"
 #include "loginMenu.h"
 #include "registerMenu.h"
+#include "Animation.h"
 #include <iostream>
 #include <filesystem>
 
@@ -31,18 +32,9 @@ int main() {
             currentScene = Scene::LoginMenu;
         });
 
-
-    sf::Texture cardTest;
-    cardTest.loadFromFile("assets/cardflip.png");
-    sf::Sprite cardSprite;
-    cardSprite.setTexture(cardTest);
-    cardSprite.setScale(10.f, 10.f);
-    cardSprite.setPosition({ 600.f, 100.f }); 
-    const int frameWidth = 64;
-    const int frameHeight = 64;
-    const int frameCount = 16;
-    int currentFrame = 0;
-    sf::Clock clock;
+    Animation cardTest("assets/cardflip.png", 64, 64, 16, 12.f,true);
+    cardTest.setPosition({ 600.f,100.f });
+    cardTest.setScale(10.f, 10.f);
 
 
     while (window.isOpen()) {
@@ -58,26 +50,18 @@ int main() {
            
         }
 
-        if (clock.getElapsedTime().asSeconds() > 0.15f) { 
-            currentFrame = (currentFrame + 1) % frameCount;
-            cardSprite.setTextureRect(sf::IntRect(currentFrame * frameWidth, 0, frameWidth, frameHeight));
-            clock.restart();
-        }
+        cardTest.update();
 
         window.clear();
 
-   
         if (currentScene == Scene::LoginMenu) {
             loginMenu.draw(window);
         }
         else if (currentScene == Scene::Register) {
             registerMenu.draw(window);
-            window.draw(cardSprite);
+            window.draw(cardTest);
         }
 
-        
-
-     
         window.display();
     }
 
