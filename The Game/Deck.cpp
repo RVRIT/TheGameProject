c8be156f8b7206e6b4bf7d15725cf8dfa66a7df1
Cristian Buraga-Vilcu <cristianburagavilcu@gmail.com>
2025-11-08 18:52:53 +0200

    Implement Deck class logic for initialization and card drawing

--- The Game/Deck.cpp
+++ The Game/Deck.cpp
@@ -1,1 +1,38 @@
 #include "Deck.h"
+#include <stdexcept>
+
+Deck::Deck()
+{
+	m_cards.reserve(98);
+
+	for(uint8_t i = 2;i <= 99;++i)
+	{ 
+		m_cards.emplace_back(i);
+	}
+
+	m_shuffler.shuffle(m_cards);
+
+}
+
+Card Deck::drawCard()
+{
+	if (isEmpty())
+	{
+		throw std::runtime_error("Cannot draw from an empty deck.");
+	}
+
+	Card topCard = m_cards.back();
+	m_cards.pop_back();
+
+	return topCard;
+}
+
+bool Deck::isEmpty() const
+{
+	return m_cards.empty();
+}
+
+size_t Deck::size() const
+{
+	return m_cards.size();
+}
