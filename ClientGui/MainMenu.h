6b1376db6b0464db2944d699c7638359d09dc2b7
Cristian Banu <cristi2005b@yahoo.com>
2025-11-17 15:24:37 +0200

    .

--- ClientGui/MainMenu.h
+++ ClientGui/MainMenu.h
@@ -1,25 +1,23 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "button.h"
-#include "Scene.h"  // <-- 1. Include Scene.h
+#include "Scene.h"  
 
-class SceneManager; // <-- 2. Forward-declare SceneManager
+class SceneManager; 
 
-class MainMenu : public Scene // <-- 3. Mo?tene?te public din Scene
+class MainMenu : public Scene 
 {
 public:
-    // 4. Constructorul prime?te SceneManager, nu callback-uri
     MainMenu(SceneManager& manager, sf::RenderWindow& window);
 
-    // 5. Implementeaz? func?iile virtuale din Scene
     virtual void handleEvent(const sf::Event& event, sf::RenderWindow& window) override;
     virtual void update(sf::Time dt) override;
-    virtual void draw(sf::RenderWindow& window) override; // <-- Numele corect
+    virtual void draw(sf::RenderWindow& window) override; 
 
     void updateBackgroundScale();
 
 private:
-    SceneManager& sceneManager; // <-- 6. Stocheaz? referin?a la manager
+    SceneManager& sceneManager; 
     sf::RenderWindow& window;
     Button playButton;
     Button exitButton;
